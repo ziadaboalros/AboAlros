cd022fae3a2cfdce43d639231997083817cfcd26
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-10-17 08:37:55 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="`${mode} smooth `">
+    <theme-toggle :mode="mode" @changeMood="changeMood" />
+    <div
+      class="min-w-64 max-w-72 mx-auto w-3/5 h-[100vh] justify-center gap-5 py-4 flex flex-col"
+    >
+      <avatar />
+      <links />
+    </div>
+  </div>
+</template>
+
+<script>
+import Avatar from "./components/avatar.vue";
+import Links from "./components/Links.vue";
+import ThemeToggle from "./components/ThemeToggle.vue";
+export default {
+  data() {
+    return {
+      mode: "bg-dark",
+    };
+  },
+  methods: {
+    changeMood(e) {
+      this.mode = e;
+    },
+  },
+
+  components: { Links, ThemeToggle, Avatar },
+};
+</script>
---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="cursor-pointer bg-cyan-800 absolute left-2 top-2 w-16 h-8 rounded-2xl p-2 flex items-center shadow-lg"
+    @click="changeMood()"
+  >
+    <div
+      :class="`w-6 rounded-full toggle aspect-square absolute left-2 bg-[#fa0] smooth ${
+        mode == 'bg-dark' ? 'active' : ''
+      }`"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    changeMood() {
+      this.$emit(
+        "changeMood",
+        this.mode == "bg-light" ? "bg-dark" : "bg-light"
+      );
+    },
+  },
+  props: ["mode"],
+};
+</script>
+
+<style scoped>
+.active {
+  transform: translateX(calc(-100% - 8px));
+  @apply bg-white left-full after:absolute after:right-0 after:top-0 after:w-4/5 after:aspect-square after:rounded-full after:bg-cyan-800;
+}
+</style>
